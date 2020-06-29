41529c29ea8e248861fc9fe2432c41dff02e4eef
Quang Thang <thang10cb9@gmail.com>
2020-06-29 15:04:05 +0700

    abc

--- src/data/data.js
+++ src/data/data.js
@@ -10,58 +10,58 @@ export default [
     "logo": "Bạn đã thoát nạn"
   },
   {
-    "fillStyle": "#FFFFFF",
-    "textFillStyle": "#303F9F",
+    "fillStyle": "#fc7800",
+    "textFillStyle": "#14c187",
     "fullName": "FC Bayern München",
     "text": "FCB",
     "logo": "Uống 100%, thêm 1 lượt quay"
   },
   {
-    "fillStyle": "#1E88E5",
+    "fillStyle": "#ba4d4e",
     "textFillStyle": "#FFFFFF",
     "fullName": "FC Barcelona",
     "text": "BAR",
     "logo": "Uống 50%"
   },
   {
-    "fillStyle": "#FFFFFF",
+    "fillStyle": "#14c187",
     "textFillStyle": "#303F9F",
     "fullName": "Club Atlético de Madrid",
     "text": "ATM",
     "logo": "Tất cả uống 100% <Đồng Khởi>"
   },
   {
-    "fillStyle": "#1E88E5",
-    "textFillStyle": "#FFFFFF",
+    "fillStyle": "#FFFFFF",
+    "textFillStyle": "#303F9F",
     "fullName": "Juventus",
     "text": "JUV",
     "logo": "Gắp mồi đút cho người đối diện"
   },
   {
-    "fillStyle": "#FFFFFF",
-    "textFillStyle": "#303F9F",
+    "fillStyle": "#1E88E5",
+    "textFillStyle": "#FFFFFF",
     "fullName": "Paris Saint-Germain",
     "text": "PSG",
     "logo": "Áo trắng và đen uống 100%"
   },
   {
-    "fillStyle": "#1E88E5",
-    "textFillStyle": "#FFFFFF",
+    "fillStyle": "#fc7800",
+    "textFillStyle": "#14c187",
     "short_name": "DOR",
     "fullName": "Borussia Dortmund",
     "text": "DOR",
     "logo": "Bên phải uống 100%, bên trái ăn mồi"
   },
   {
-    "fillStyle": "#FFFFFF",
-    "textFillStyle": "#303F9F",
+    "fillStyle": "#ba4d4e",
+    "textFillStyle": "#FFFFFF",
     "fullName": "Sevilla FC",
     "text": "SEV",
     "logo": "Bên trái uống 50% "
   },
   {
-    "fillStyle": "#1E88E5",
-    "textFillStyle": "#FFFFFF",
+    "fillStyle": "#14c187",
+    "textFillStyle": "#303F9F",
     "fullName": "SL Benfica",
     "text": "BEN",
     "logo": "Uống 100% với người chỉ định "
@@ -81,64 +81,64 @@ export default [
     "logo": "Áo màu uống 100%"
   },
   {
-    "fillStyle": "#FFFFFF",
-    "textFillStyle": "#303F9F",
+    "fillStyle": "#fc7800",
+    "textFillStyle": "#14c187",
     "fullName": "Manchester City FC",
     "text": "MANC",
     "logo": "Uống 100% với 2 người kế bên"
   },
   {
-    "fillStyle": "#1E88E5",
+    "fillStyle": "#ba4d4e",
     "textFillStyle": "#FFFFFF",
     "fullName": "FC Schalke 04",
     "text": "S04",
     "logo": "Uống 200% (Không uống hát 1 bài) "
   },
   {
-    "fillStyle": "#FFFFFF",
+    "fillStyle": "#14c187",
     "textFillStyle": "#303F9F",
     "fullName": "Manchester United FC",
     "text": "MANU",
     "logo": " Bên phải uống 100%, đối diện ăn ớt "
   },
   {
-    "fillStyle": "#1E88E5",
-    "textFillStyle": "#FFFFFF",
+    "fillStyle": "#FFFFFF",
+    "textFillStyle": "#303F9F",
     "fullName": "Bayer 04 Leverkusen",
     "text": "BAYER",
     "logo": "Bên phải uống 50%"
   },
   {
-    "fillStyle": "#FFFFFF",
-    "textFillStyle": "#303F9F",
+    "fillStyle": "#1E88E5",
+    "textFillStyle": "#FFFFFF",
     "fullName": "SSC Napoli",
     "text": "NAP",
     "logo": "Thưởng được uống 100%"
   },
   {
-    "fillStyle": "#FFFFFF",
-    "textFillStyle": "#303F9F",
+    "fillStyle": "#fc7800",
+    "textFillStyle": "#14c187",
     "fullName": "Tottenham Hotspur FC",
     "text": "TOT",
     "logo": "Chọn người uống cùng 100%"
   },
   {
-    "fillStyle": "#1E88E5",
+    "fillStyle": "#ba4d4e",
     "textFillStyle": "#FFFFFF",
     "fullName": "Valencia CF",
     "text": "VAL",
     "logo": "Chọn người uống cùng 50%"
   },
   {
-    "fillStyle": "#FFFFFF",
+    "fillStyle": "#14c187",
     "textFillStyle": "#303F9F",
     "fullName": "Olympique Lyonnais",
     "text": "LYO",
     "logo": "Cho 1 câu đố , chỉ định người"
   },
   {
-    "fillStyle": "#1E88E5",
-    "textFillStyle": "#FFFFFF",
+    "fillStyle": "#FFFFFF",
+    "textFillStyle": "#303F9F",
     "short_name": "FIO",
     "fullName": "ACF Fiorentina",
     "text": "FIO",
